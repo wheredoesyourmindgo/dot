b68166cf4eae4ebae12e0cad25ae611baac4f14a
wheredoesyourmindgo <moemanchu@icloud.com>
2021-03-21 10:58:33 -0700

    Added install_all.sh

---
+++ install_all.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cp hyper/.hyper.js ~/
+cp vim/.vimrc vim/.gvimrc ~/
+cp zsh/.zshrc zsh/.zshenv zsh/.zprofile ~/
+cp ansiweather/.ansiweatherrc ~/
+cp karabiner/karabiner.json ~/.config/karabiner/
