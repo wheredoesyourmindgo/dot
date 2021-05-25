09b8302c24f36c9045daf9958a119ad328780e05
pcwa-ahendricks <ahendricks@pcwa.net>
2021-05-25 07:13:50 -0700

    Fix copy script

--- install_all.sh
+++ install_all.sh
@@ -3,5 +3,5 @@ cp hyper/.hyper.js ~/
 cp vim/.vimrc vim/.gvimrc ~/
 cp zsh/.zshrc zsh/.zshenv zsh/.zprofile ~/
 cp ansiweather/.ansiweatherrc ~/
-[ -d "$HOME/.ssh/config" ] && cp .ssh/config ~/.ssh/
+[ -d "$HOME/.ssh" ] && cp .ssh/config ~/.ssh/
 [ -d "$HOME/.config/karabiner" ] && cp karabiner/karabiner.json ~/.config/karabiner/
