b8be93402d2ffae8bfbd5b2bd22753e5e81e51ba
Abe Hendricks <ahendricks@pcwa.net>
2021-05-26 09:18:42 -0700

    Added custom services

--- install_all.sh
+++ install_all.sh
@@ -5,3 +5,4 @@ cp zsh/.zshrc zsh/.zshenv zsh/.zprofile ~/
 cp ansiweather/.ansiweatherrc ~/
 [ -d "$HOME/.ssh" ] && cp .ssh/config ~/.ssh/
 [ -d "$HOME/.config/karabiner" ] && cp karabiner/karabiner.json ~/.config/karabiner/
+[ -d "$HOME/Library/Services" ] && cp Library/Services/* ~/Library/Services/
