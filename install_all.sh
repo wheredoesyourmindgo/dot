2688192dd672b7f7e6515cb03b2526932a693e4a
wheredoesyourmindgo <moemanchu@icloud.com>
2021-06-03 09:35:42 -0700

    Added Starship config

--- install_all.sh
+++ install_all.sh
@@ -5,5 +5,6 @@ cp zsh/.zshrc zsh/.zshenv zsh/.zprofile ~/
 cp ansiweather/.ansiweatherrc ~/
 [ -d "$HOME/.ssh" ] && cp .ssh/config ~/.ssh/
 [ -d "$HOME/.config/karabiner" ] && cp karabiner/karabiner.json ~/.config/karabiner/
+[ -d "$HOME/.config" ] && cp starship/starship.toml ~/.config/
 [ -d "$HOME/Library/Services" ] && cp -R Library/Services/* ~/Library/Services/
 [ -d "$HOME/Library/Application Support/iTerm2/Scripts" ] && cp -R iterm/scripts/* ~/Library/Application\ Support/iTerm2/Scripts/
